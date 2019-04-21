717e28fefbb485682eaa6262dd818cc07c8bdd0c
hemy08 <zhaojunwei008@yeah.net>
2019-04-21 16:28:02 +0800

    CMAKE
    Signed-off-by: hemy08 <zhaojunwei008@yeah.net>

---
+++ cmake-build-debug/CMakeFiles/SortAlgorithm.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/myCodeHub/SortAlgorithm/SortTest/Debug.c" "E:/myCodeHub/SortAlgorithm/cmake-build-debug/CMakeFiles/SortAlgorithm.dir/SortTest/Debug.c.obj"
+  "E:/myCodeHub/SortAlgorithm/SortTest/SortTest.c" "E:/myCodeHub/SortAlgorithm/cmake-build-debug/CMakeFiles/SortAlgorithm.dir/SortTest/SortTest.c.obj"
+  "E:/myCodeHub/SortAlgorithm/main.c" "E:/myCodeHub/SortAlgorithm/cmake-build-debug/CMakeFiles/SortAlgorithm.dir/main.c.obj"
+  "E:/myCodeHub/SortAlgorithm/src/Bubblesort.c" "E:/myCodeHub/SortAlgorithm/cmake-build-debug/CMakeFiles/SortAlgorithm.dir/src/Bubblesort.c.obj"
+  "E:/myCodeHub/SortAlgorithm/src/Bucketsort.c" "E:/myCodeHub/SortAlgorithm/cmake-build-debug/CMakeFiles/SortAlgorithm.dir/src/Bucketsort.c.obj"
+  "E:/myCodeHub/SortAlgorithm/src/Combsort.c" "E:/myCodeHub/SortAlgorithm/cmake-build-debug/CMakeFiles/SortAlgorithm.dir/src/Combsort.c.obj"
+  "E:/myCodeHub/SortAlgorithm/src/Comparisonsort.c" "E:/myCodeHub/SortAlgorithm/cmake-build-debug/CMakeFiles/SortAlgorithm.dir/src/Comparisonsort.c.obj"
+  "E:/myCodeHub/SortAlgorithm/src/Heapsort.c" "E:/myCodeHub/SortAlgorithm/cmake-build-debug/CMakeFiles/SortAlgorithm.dir/src/Heapsort.c.obj"
+  "E:/myCodeHub/SortAlgorithm/src/Introsort.c" "E:/myCodeHub/SortAlgorithm/cmake-build-debug/CMakeFiles/SortAlgorithm.dir/src/Introsort.c.obj"
+  "E:/myCodeHub/SortAlgorithm/src/Mergesort.c" "E:/myCodeHub/SortAlgorithm/cmake-build-debug/CMakeFiles/SortAlgorithm.dir/src/Mergesort.c.obj"
+  "E:/myCodeHub/SortAlgorithm/src/Quicksort.c" "E:/myCodeHub/SortAlgorithm/cmake-build-debug/CMakeFiles/SortAlgorithm.dir/src/Quicksort.c.obj"
+  "E:/myCodeHub/SortAlgorithm/src/Radixsort.c" "E:/myCodeHub/SortAlgorithm/cmake-build-debug/CMakeFiles/SortAlgorithm.dir/src/Radixsort.c.obj"
+  "E:/myCodeHub/SortAlgorithm/src/Timsort.c" "E:/myCodeHub/SortAlgorithm/cmake-build-debug/CMakeFiles/SortAlgorithm.dir/src/Timsort.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SortAlgorithm.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SortAlgorithm.dir/main.c.obj"
+  "CMakeFiles/SortAlgorithm.dir/src/Bubblesort.c.obj"
+  "CMakeFiles/SortAlgorithm.dir/src/Bucketsort.c.obj"
+  "CMakeFiles/SortAlgorithm.dir/src/Combsort.c.obj"
+  "CMakeFiles/SortAlgorithm.dir/src/Comparisonsort.c.obj"
+  "CMakeFiles/SortAlgorithm.dir/src/Heapsort.c.obj"
+  "CMakeFiles/SortAlgorithm.dir/src/Introsort.c.obj"
+  "CMakeFiles/SortAlgorithm.dir/src/Mergesort.c.obj"
+  "CMakeFiles/SortAlgorithm.dir/src/Quicksort.c.obj"
+  "CMakeFiles/SortAlgorithm.dir/src/Radixsort.c.obj"
+  "CMakeFiles/SortAlgorithm.dir/src/Timsort.c.obj"
+  "CMakeFiles/SortAlgorithm.dir/SortTest/SortTest.c.obj"
+  "CMakeFiles/SortAlgorithm.dir/SortTest/Debug.c.obj"
+  "SortAlgorithm.pdb"
+  "SortAlgorithm.exe"
+  "SortAlgorithm.exe.manifest"
+  "libSortAlgorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SortAlgorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
